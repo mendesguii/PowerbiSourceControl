ce4b1a545c40272a5aa5171a3ed42d3703ba1d40
Guilherme Mendes <guilhermemendes789@gmail.com>
2023-05-30 12:15:48 +0100

    fix #2 for the - on the directory

--- $scripts/CompilePBIT.bat
+++ $scripts/CompilePBIT.bat
@@ -1,6 +1,6 @@
 @echo off
-set FILE=%1
+set "FILE=%1"
 setlocal
 set "batch_dir=%~dp0"
-pbi-tools compile "%FILE%" -outPath %batch_dir% -format "PBIT"
+pbi-tools compile %FILE% -outPath %batch_dir% -format "PBIT"
 pause
